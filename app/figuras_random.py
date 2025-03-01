3ef84298ba163dc474761ba1e8329ef0679e0233
Mauro M.G.V. <isc.manuelgv@gmail.com>
2025-03-01 11:40:24 -0600

    implementacion de pentagono y colores

--- app/figuras_random.py
+++ app/figuras_random.py
@@ -2,12 +2,14 @@ import json,random
 
 def figuras_random():
     figuras=[]
-    nombres = ["cuadrado", "triangulo", "circulo"]
+    nombres = ["cuadrado", "triangulo", "circulo", "pentagono"]
+    colores = ["rojo", "verde", "azul", "amarillo", "negro", "blanco", "gris", "morado", "naranja", "rosa", "marrón"]
     for i in range(0, random.randint(2, 10)):
         figuras.append({
-            "nombre": nombres[random.randint(0, 2)],
+            "nombre": nombres[random.randint(0, 3)],
             "x": random.randint(0, 400),
             "y": random.randint(0, 400),
-            "medida": random.randint(0, 100)
+            "medida": random.randint(0, 100),
+            "color": colores[random.randint(0, 9)]
         })
     return json.dumps(figuras)
