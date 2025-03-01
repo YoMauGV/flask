982e6ba5d09dcf8fda4f6eac0d01f75eac75a26f
Mauro M. G. V <isc.manuelgv@gmail.com>
2025-03-01 14:23:11 -0600

    Actualización de colores según QT

--- app/figuras_random.py
+++ app/figuras_random.py
@@ -3,13 +3,13 @@ import json,random
 def figuras_random():
     figuras=[]
     nombres = ["cuadrado", "triangulo", "circulo", "pentagono"]
-    colores = ["rojo", "verde", "azul", "amarillo", "negro", "blanco", "gris", "morado", "naranja", "rosa", "marrón"]
+    colores = ["blanco", "negro", "rojo", "rojo oscuro", "verde", "verde oscuro", "azul", "azul oscuro", "cian", "cian oscuro", "magenta", "magenta oscuro", "amarillo", "amarillo oscuro", "gris", "gris oscuro", "gris claro"]
     for i in range(0, random.randint(2, 10)):
         figuras.append({
             "nombre": nombres[random.randint(0, 3)],
             "x": random.randint(0, 400),
             "y": random.randint(0, 400),
             "medida": random.randint(0, 100),
-            "color": colores[random.randint(0, 9)]
+            "color": colores[random.randint(0, 16)]
         })
     return json.dumps(figuras)
