346bd9f8b3a3bd4cb15ee13f534d20c06e60bfc2
Mauro M. G. V <isc.manuelgv@gmail.com>
2025-03-01 14:32:57 -0600

    se quito transparente y blanco

--- app/figuras_random.py
+++ app/figuras_random.py
@@ -3,13 +3,13 @@ import json,random
 def figuras_random():
     figuras=[]
     nombres = ["cuadrado", "triangulo", "circulo", "pentagono"]
-    colores = ["blanco", "negro", "rojo", "rojo oscuro", "verde", "verde oscuro", "azul", "azul oscuro", "cian", "cian oscuro", "magenta", "magenta oscuro", "amarillo", "amarillo oscuro", "gris", "gris oscuro", "gris claro"]
+    colores = ["negro", "rojo", "rojo oscuro", "verde", "verde oscuro", "azul", "azul oscuro", "cian", "cian oscuro", "magenta", "magenta oscuro", "amarillo", "amarillo oscuro", "gris", "gris oscuro", "gris claro"]
     for i in range(0, random.randint(2, 10)):
         figuras.append({
             "nombre": nombres[random.randint(0, 3)],
-            "x": random.randint(0, 400),
-            "y": random.randint(0, 400),
+            "x": random.randint(0, 700),
+            "y": random.randint(0, 700),
             "medida": random.randint(0, 100),
-            "color": colores[random.randint(0, 16)]
+            "color": colores[random.randint(0, 15)]
         })
     return json.dumps(figuras)
