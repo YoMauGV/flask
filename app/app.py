f98fc52e4ac3aafb4dc22be1f31b88f212bcd087
Mauro M. G. V <isc.manuelgv@gmail.com>
2025-03-10 07:41:19 -0600

    Imagemagick y Wand implementados

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import json
 import figuras_random
-from flask import Flask, json, render_template
+import dibuja
+from flask import Flask, json, render_template, Response
 app = Flask(__name__)
 
 @app.route('/', methods=('GET', 'POST'))
@@ -29,6 +30,12 @@ def github():
 @app.route('/figuras_random', methods=('GET', 'POST'))
 def fr():
     return figuras_random.figuras_random()
+
+@app.route('/dibuja', methods=('GET', 'POST'))
+def db():
+    r = Response(response=dibuja.dibuja(), status=200, mimetype="image/png")
+    r.headers["Content-Type"] = "image/png"
+    return r
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
